123052d161ad33424fa653fcd3704b82df1528e1
Matej Brezničan <148859673+UnizaMatBre@users.noreply.github.com>
2025-04-20 19:57:38 +0200

    feat: Added token pulling into parser

--- source/compiler/parsing.py
+++ source/compiler/parsing.py
@@ -2,5 +2,18 @@
 
 
 class Parser:
-    def __init__(self):
+    def __init__(self, tokens):
+        self._tokens = tokens
+        self._tokens_index = 0
+
+
+    def _pull_token(self):
+        """Returns token and moved index forward"""
+        prev_index = self._tokens_index
+        self._tokens_index += 1
+
+        return self._tokens[prev_index]
+
+
+    def parse_any(self):
         pass
